bea0f33a7376d1914ff91e4b5062b9d473b8f313
Roman <mrmarshinov@gmail.com>
2025-09-20 13:17:15 +0200

    first commit

---
+++ Wokrspace switch/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import subprocess as sub
+
+# Set your target workspace here starting from 1
+target_workspace = 3
+
+# Read you previous workspace
+with open("current.txt", "r") as f:
+    previous_workspace = f.read()
+
+# Checking opened workspaces
+workspaces = sub.check_output(["wmctrl","-d"], text=True)
+workspace_list = workspaces.split("\n")
+number_of_workspaces = len(workspace_list) - 2
+# Create target workspace if it necessary
+if target_workspace > number_of_workspaces:
+    sub.run(["wmctrl","-n",f"{target_workspace}"])
+
+# Check current workspace
+for i, workspace in enumerate(workspace_list):
+    if "*" in workspace:
+        current_workspace = i
+
+# Switch to target workspace if needed or switch to previous
+if current_workspace != target_workspace:
+    sub.run(['wmctrl',"-s", f"{target_workspace }"])
+    with open("current.txt", "w") as f:
+        f.write(f"{current_workspace}")
+else:
+    sub.run(['wmctrl', "-s", f"{previous_workspace}"])
