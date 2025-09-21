78a190fa2ee84d69c00ee6483f60d98ec87983bd
Roman <mrmarshinov@gmail.com>
2025-09-21 18:13:15 +0200

    upd

--- Wokrspace switch/main.py
+++ Wokrspace switch/main.py
@@ -2,31 +2,33 @@
 import subprocess as sub
 
 # Set your target workspace here starting from 1
-target_workspace = 2
+target_workspace = 5
 
 # Read you previous workspace
 try:
     with open("current.txt", "r") as f:
-        previous_workspace = f.read()
+        previous_workspace = int(f.read())
 except FileNotFoundError:
     previous_workspace = 1
+
 # Checking opened workspaces
 workspaces = sub.check_output(["wmctrl","-d"], text=True)
-workspace_list = workspaces.split("\n")
-number_of_workspaces = len(workspace_list) - 2
+workspace_list = workspaces.strip().split("\n")
+number_of_workspaces = len(workspace_list)
+
 # Create target workspace if it necessary
 if target_workspace > number_of_workspaces:
-    sub.run(["wmctrl","-n",f"{target_workspace + 1}"])
+    sub.run(["wmctrl","-n",f"{target_workspace}"])
 
 # Check current workspace
 for i, workspace in enumerate(workspace_list):
     if "*" in workspace:
-        current_workspace = i
+        current_workspace = i + 1
 
 # Switch to target workspace if needed or switch to previous
 if current_workspace != target_workspace:
-    sub.run(['wmctrl',"-s", f"{target_workspace }"])
+    sub.run(['wmctrl',"-s", f"{target_workspace - 1}"])
     with open("current.txt", "w") as f:
         f.write(f"{current_workspace}")
 else:
-    sub.run(['wmctrl', "-s", f"{previous_workspace}"])
+    sub.run(['wmctrl', "-s", f"{previous_workspace - 1}"])
